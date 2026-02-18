f7195f979859d2b0ef217a6adf6a3e90161418c4
robinkneip2002-crypto <>
2026-02-18 18:25:21 +0000

    Variable Naming

---
+++ variablen_und_typen/variable_naming/main.py
@@ -0,0 +1,11 @@
+item_name = "Bread"
+_item_price = 4.52
+item_in_stock  = 230
+
+
+
+
+# Testing
+print("Item:", item_name)
+print("Price per loaf: $", _item_price)
+print("Quantity in stock:", item_in_stock)

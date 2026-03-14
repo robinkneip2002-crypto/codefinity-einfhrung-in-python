0844191c7a9281bb5e6c1a9b811286d969145f51
robinkneip2002-crypto <>
2026-03-14 09:35:19 +0000

    Combining Conditions

---
+++ bedingte_anweisungen/combining_conditions/main.py
@@ -0,0 +1,12 @@
+# The item's discount and stock status have been defined
+discounted = False
+lowStock = True
+
+movingProduct = discounted or lowStock
+
+promotion = not movingProduct
+
+
+
+
+print ("!Is the item eligible for promotion?", promotion)
